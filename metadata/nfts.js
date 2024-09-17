34c166fd79564710f25fda737c1e91047e505cbd
NikitaJha16 <139324050+NikitaJha16@users.noreply.github.com>
2024-09-18 01:32:54 +0530

    Create nfts.js

---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "this is my jharkhand",
+    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+  },
+  {
+    name: "1",
+    description: "this is my jharkhand",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, //change
+  {
+    name: "2",
+    description: "this is my jharkhand",
+    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+  },
+  {
+    name: "3",
+    description: "this is my jharkhand",
+    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+  },
+  {
+    name: "4",
+    description: "this is my jharkhand.",
+    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+  },
+];
+
+module.exports = nfts;
